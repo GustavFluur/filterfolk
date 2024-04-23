cfb50fd985a6bf86a99d45105a2a36282acafd5e
GustavFluur <gustavfluur@hotmail.com>
2024-04-23 19:42:08 +0000

    Added new features by adding relating product function from another walkthrough project

--- product/views.py
+++ product/views.py
@@ -7,9 +7,9 @@ from .models import Product
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    #related_merchandises = product.objects.filter(category=merchandise.category, is_sold=False).exclude(pk=pk)[0:3]
+    related_products = Product.objects.filter(category=product.category).exclude(pk=pk)[0:3]
 
     return render(request, 'product/product_detail.html', {
         'product': product,
-        #'related_merchandises': related_merchandises
+        'related_products': related_products
     })
