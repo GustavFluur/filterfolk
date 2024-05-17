599b24118a981df28411878c455778bac1485f6c
GustavFluur <gustavfluur@hotmail.com>
2024-05-17 21:01:15 +0000

    Added adjustmnets and valuable content into the cart function

---
+++ cart/contexts.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
