ff5200a6bb2792a103c1434a97af408d78633d43
GustavFluur <gustavfluur@hotmail.com>
2024-05-20 10:12:33 +0000

    added changes for the cart app and its file by extracting code from another walkthrough project

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
+    path('add/', views.add_cart, name='add_cart'),
+    path('update/', views.update_cart, name='update_cart'),
+    path('delete/', views.delete_cart, name='delete_cart'),
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -6,19 +6,15 @@ def view_cart(request):
 
     return render(request, 'cart/cart.html')
 
-def add_to_cart(request, item_id):
+def add_cart(request, item_id):
+    pass
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+def update_cart(request, item_id):
 
-    cart = request.session.get('cart', {})
+    pass
 
-    if item_id in list(cart.keys()):
-            cart[item_id] += quantity
-            messages.success(
-                request, f'Updated {product.name} quantity to {cart[item_id]}')
-    else:
-            cart[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your cart')
+
+def delete_cart(request, item_id):
+
+    pass
 
