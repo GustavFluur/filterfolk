090d18a11030632f08b8d6acac2ca0cae5584bae
GustavFluur <gustavfluur@hotmail.com>
2024-05-21 10:18:41 +0000

    Added content and features into the cart app

--- cart/cart.py
+++ cart/cart.py
@@ -12,3 +12,28 @@ class Cart():
 
 		self.cart = cart
 
+def add(self, product, quantity):
+		product_id = str(product.id)
+		product_qty = str(quantity)
+		# Logic
+		if product_id in self.cart:
+			pass
+		else:
+			#self.cart[product_id] = {'price': str(product.price)}
+			self.cart[product_id] = int(product_qty)
+
+		self.session.modified = True
+
+		# Deal with logged in user
+		if self.request.user.is_authenticated:
+			# Get the current user profile
+			current_user = Profile.objects.filter(user__id=self.request.user.id)
+			# Convert {'3':1, '2':4} to {"3":1, "2":4}
+			carty = str(self.cart)
+			carty = carty.replace("\'", "\"")
+			# Save carty to the Profile Model
+			current_user.update(old_cart=str(carty))
+
+
+def __len__(self):
+		return len(self.cart)
--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .cart import Cart
+from product.models import Product
+from django.http import JsonResponse
 from django.contrib import messages
+
 
 
 def view_cart(request):
 
     return render(request, 'cart/cart.html')
 
+
+
 def add_cart(request, item_id):
-    pass
+    cart = Cart(request)
+	# test for POST
+	
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, quantity=product_qty)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, ("Product Added To Shopping Cart..."))
+    
+    return response
+
 
 def update_cart(request, item_id):
 
