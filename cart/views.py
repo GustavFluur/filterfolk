0b0717ccf2831b46a0c072c5dd1efa06dfd42220
GustavFluur <gustavfluur@hotmail.com>
2024-05-23 17:38:57 +0000

    Added content and features into template, views.py in cart app and some changes in the product app

--- cart/views.py
+++ cart/views.py
@@ -30,7 +30,7 @@ def update_cart(request, item_id):
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+        cart[item_id] = quantity
     else:
         cart[item_id] = quantity
 
