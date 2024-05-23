920e37c140f8bbf0bd923f3b0ff7023ba216aa26
GustavFluur <gustavfluur@hotmail.com>
2024-05-23 13:38:49 +0000

    Added feautures in the code to increement the products plus working on how to remove them

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.cart_total, name='cart_total'),
     path('add/<item_id>', views.add_cart, name='add_cart'),
-    path('update/', views.update_cart, name='update_cart'),
-    path('delete/', views.delete_cart, name='delete_cart'),
+    path('update/<item_id>', views.update_cart, name='update_cart'),
+    path('delete/<item_id>', views.delete_cart, name='delete_cart'),
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from product.models import Product
 from django.contrib import messages
 
@@ -26,10 +26,29 @@ def add_cart(request, item_id):
 
 def update_cart(request, item_id):
 
-    pass
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse("cart_total"))
 
 
 def delete_cart(request, item_id):
 
-    pass
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
 
