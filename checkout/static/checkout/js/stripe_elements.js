834022691dae867b7b1d19947ce51143e88c0d34
GustavFluur <gustavfluur@hotmail.com>
2024-06-12 20:27:44 +0000

    Added webhook functionalities, stripe element js and views.py from BA

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -123,6 +123,6 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         console.log('stripe call failed')
         // Just reload the page, the error will be in django messages
-        //location.reload();
+        location.reload();
     });
 });
