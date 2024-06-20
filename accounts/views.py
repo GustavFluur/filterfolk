e29621309275999df5b0288283d7f3db0eb024dd
Gustav Fluur <gustavfluur@hotmail.com>
2024-06-20 19:48:54 +0000

    Added code from Boutique ado into accounts app for profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserAccount
 from .forms import UserAccountForm
@@ -8,13 +9,20 @@ def account(request):
 
     account = get_object_or_404(UserAccount, user=request.user)
 
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Personal account updated successfully')
+
     form = UserAccountForm(instance=account)
     orders = account.orders.all() 
 
     template = 'accounts/account.html'
     context = {
         'form': form,
-        'orders': orders,  
+        'orders': orders,
+        'on_account_page': True  
         
     }
 
