f50b6d9d9498eded7a8e7cd963ae34751a940a90
Gustav Fluur <gustavfluur@hotmail.com>
2024-06-24 17:49:40 +0000

    Added requirements for the database in Elephant SQL + heroku

--- filter_folk/settings.py
+++ filter_folk/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,12 +116,13 @@ WSGI_APPLICATION = 'filter_folk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
